0cc658560ab6452924a1a6ef6c6bc7e6f21642fd
Lauty Vallino <141865628+LautyUCC@users.noreply.github.com>
2024-05-14 16:18:07 -0300

    Union de Home, About y Skills
    erros en skills con las progress bar a solucionar

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["System engineer student", "photographer", "Junior developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
