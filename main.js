cb0549849b7cd61e8f4fe4ee7a6a96edd68b7c60
Lauty Vallino <141865628+LautyUCC@users.noreply.github.com>
2024-05-21 00:04:09 -0300

    Pelado como gallina
    mejora en skills y ahora en el head se ven todas las opciones

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["System engineer student", "photographer", "Junior developer"],
+    strings: ["Photographer", "System Engineer Student", "Junior developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
